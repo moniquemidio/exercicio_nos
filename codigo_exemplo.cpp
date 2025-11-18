cd67d5dd91479e432311fa81a6c4dc345c0b2c76
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2025-11-17 23:20:31 -0300

    Create codigo_exemplo.cpp

---
+++ codigo_exemplo.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int dado;
+    Node* prox;
+};
+
+void inserirInicio(Node*& head, int valor) {
+    Node* novo = new Node{valor, head};
+    head = novo;
+}
+
+void imprimirLista(Node* head) {
+    Node* atual = head;
+
+    while (atual != nullptr) {
+        cout << atual->dado << " -> ";
+        atual = atual->prox;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+
+    Node* head = new Node{30, nullptr};
+    head = new Node{15, head};
+
+    cout << "Lista inicial:" << endl;
+    imprimirLista(head);
+
+    inserirInicio(head, 50);
+
+    cout << "\nLista após inserir 50 no início:"< endl;
+    imprimirLista(head);
+
+    return 0;
+}
